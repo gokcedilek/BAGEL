09aca2858fe58bdbbcb1df59385499b083953f5b
JH Kim <ehwo78@gmail.com>
2022-09-17 11:19:09 -0700

    init db setup

--- cmd/database/main.go
+++ cmd/database/main.go
@@ -1,49 +1,58 @@
 package main
 
 import (
-	"io"
-	"log"
-	"os"
+	"context"
 	"project/database"
-	"project/util"
 )
 
 const (
-	PROD = "prod"
-	DEV  = "dev"
+	PROD  = "prod"
+	DEV   = "dev"
+	SETUP = "setup"
 )
 
 func main() {
 
-	logFile, err := os.OpenFile("bagel.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logFile.Close()
-	mw := io.MultiWriter(os.Stdout, logFile)
-	log.SetOutput(mw)
-	log.SetPrefix("Database" + ": ")
+	svc := database.GetDynamoClient()
+	database.InitBagelTable(context.TODO(), svc)
 
-	if len(os.Args) != 2 || os.Args[1] != DEV && os.Args[1] != PROD {
-		log.Printf("Usage: ./bin/database [%s|%s]", PROD, DEV)
-		log.Println("Example: ./bin/database dev")
-		return
+	vertex := database.Vertex{
+		ID:    0,
+		Edges: []uint64{1, 2, 3},
+		Hash:  0,
 	}
 
-	var configPath string
-	if os.Args[1] == PROD {
-		configPath = "config/database_config.json"
-	} else {
-		configPath = "config/database_test_config.json"
-	}
+	database.InsertVertex(svc, "bagel-db", vertex)
 
-	var config database.DatabaseConfig
-	err = util.ReadJSONConfig(configPath, &config)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	database.SetupDatabase(config)
+	//logFile, err := os.OpenFile("bagel.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer logFile.Close()
+	//mw := io.MultiWriter(os.Stdout, logFile)
+	//log.SetOutput(mw)
+	//log.SetPrefix("Database" + ": ")
+	//
+	//if len(os.Args) != 2 || os.Args[1] != DEV && os.Args[1] != PROD && os.Args[1] != SETUP {
+	//	log.Printf("Usage: ./bin/database [%s|%s|%s]", PROD, DEV, SETUP)
+	//	log.Println("Example: ./bin/database dev")
+	//	return
+	//}
+	//
+	//var configPath string
+	//if os.Args[1] == PROD {
+	//	configPath = "config/database_config.json"
+	//} else {
+	//	configPath = "config/database_test_config.json"
+	//}
+	//
+	//var config database.DatabaseConfig
+	//err = util.ReadJSONConfig(configPath, &config)
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//database.SetupDatabase(config)
 
 }
