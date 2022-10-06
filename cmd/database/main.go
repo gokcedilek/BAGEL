02d4cd671b70d8b7e8c7c74c34a8aaf75f29a619
JH Kim <ehwo78@gmail.com>
2022-10-05 17:06:43 -0700

    database create partition, get partition

--- cmd/database/main.go
+++ cmd/database/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"os"
 	"project/database"
 )
 
@@ -9,44 +11,24 @@ const (
 	PROD  = "prod"
 	DEV   = "dev"
 	SETUP = "setup"
+	BAGEL = "bagel-test"
 )
 
 func main() {
+	logFile, err := os.OpenFile("bagel.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+	mw := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(mw)
+	log.SetPrefix("Database" + ": ")
+
+	if len(os.Args) != 3 {
+		log.Printf("Usage: ./bin/database [$1 TABLE_NAME] [$2<PATH_TO_GRAPH.txt>]")
+		return
+	}
 
 	svc := database.GetDynamoClient()
-	//database.AddGoogleGraphBagel(filepath.Join(util.GetProjectRoot(), "web-Google.txt"), svc)
-
-	fmt.Println(database.IsPartitionCached(svc, "bagel-db", 2))
-	//database.GetVertexByID(svc, 209963, "bagel-db")
-	//logFile, err := os.OpenFile("bagel.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer logFile.Close()
-	//mw := io.MultiWriter(os.Stdout, logFile)
-	//log.SetOutput(mw)
-	//log.SetPrefix("Database" + ": ")
-	//
-	//if len(os.Args) != 2 || os.Args[1] != DEV && os.Args[1] != PROD && os.Args[1] != SETUP {
-	//	log.Printf("Usage: ./bin/database [%s|%s|%s]", PROD, DEV, SETUP)
-	//	log.Println("Example: ./bin/database dev")
-	//	return
-	//}
-	//
-	//var configPath string
-	//if os.Args[1] == PROD {
-	//	configPath = "config/database_config.json"
-	//} else {
-	//	configPath = "config/database_test_config.json"
-	//}
-	//
-	//var config database.DatabaseConfig
-	//err = util.ReadJSONConfig(configPath, &config)
-	//
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//database.SetupDatabase(config)
-
+	database.GetPartitionForWorkerX(svc, BAGEL, 2, 0)
 }
